37645d6d228f076e8b50c6952fea0706508939eb
Vveczx <joao.turella@escola.pr.gov.br>
2025-03-24 08:24:19 -0300

    Add files via upload

--- 2025-3b-acessibilidade-main/script.js
+++ 2025-3b-acessibilidade-main/script.js
@@ -1,8 +1,24 @@
-const aumentarFonteBotao=document.getElementById (aumentar-fonte)
+document.addEventListener(`DOMContentLoaded`, function (){
+const botaoAcessibilidade=document.getElementById (`botao-acessibilidade`);
+const opcoesAcesibilidade=document.getElementById (`apresenta-lista`);
 
-let tamanhoFonteatual=1;
+botaoAcessibilidade.addEventListener(`click`, function(){
+botaoAcessibilidade.classList.toggle(`rotacao-menu`)
+opcoesAcesibilidade.classList.toggle(`apresenta-ĺista`)
+})
+const aumentarFonteBotao= document.getElementById (`aumentar-fonte`);
+const diminuirFonteBotao= document.getElementById (`diminuir-fonte`)
 
-aumentarFonteBotao.addEventListener('click', unction () {
+let tamanhoFonteatual= 1;
+
+aumentarFonteBotao.addEventListener('click', function () {
 tamanhoFonteatual += 0.1;
 document.body.style.fontSize = `${tamanhoFonteatual}rem`
 })
+
+diminuirFonteBotao.addEventListener('click', function () {
+    tamanhoFonteatual -= 0.1;
+    document.body.style.fontSize=  `${tamanhoFonteatual}rem`
+
+})
+});
